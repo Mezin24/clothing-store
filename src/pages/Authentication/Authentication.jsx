20eeff96802e83dd1d5e25415444022a69b44bc3
Pavel Mezencev <mezencev24@mail.ru>
2023-08-27 10:48:26 +0300

    add auth forms

---
+++ src/pages/Authentication/Authentication.jsx
@@ -0,0 +1,14 @@
+import { SignInForm } from "components/signInForm/SignInForm"
+import { SignUpForm } from "components/signUpForm/SignUpForm"
+import './authentication.scss'
+
+
+export const Authentication = () => {
+
+  return (
+    <div className="auth-container">
+    <SignInForm />
+    <SignUpForm />
+    </div>
+  )
+}
