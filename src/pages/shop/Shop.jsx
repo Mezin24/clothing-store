9da38d7e587790ca2611aac0289e1fd106b0ab86
Pavel Mezencev <mezencev24@mail.ru>
2023-09-01 22:42:40 +0300

    refactor to async thunk

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,31 +1,34 @@
+import { Spinner } from "components/UI/spinner/Spinner"
 import { ProductCard } from "components/productCard/ProductCard"
 import { memo } from "react"
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
-import { getCategoriesMap } from "store/categories/categories.selectors"
+import { getCategoriesIsLoading, getCategoriesMap } from "store/categories/categories.selectors"
 
 export const Shop = memo(() => {
   const categoriesMap = useSelector(getCategoriesMap)
-
-  if (!categoriesMap) return null
+  const categoriesIsLoading = useSelector(getCategoriesIsLoading)
 
   return (
     <>
-      {Object.entries(categoriesMap).map(([title, products]) => (
-        <div key={title}>
-          <h2>
-            <Link to={`${title}`}>
-                {title.toUpperCase()}
-            </Link>
-          </h2>
-          <div className="products-container">
-            {[...products]
-              .splice(0, 4)
-              .map((product) => (
-              <ProductCard key={product.id} product={product}/>
-            ))}
+      {
+      categoriesIsLoading ?
+        <Spinner/> : 
+        Object.entries(categoriesMap).map(([title, products]) => (
+          <div key={title}>
+            <h2>
+              <Link to={`${title}`}>
+                  {title.toUpperCase()}
+              </Link>
+            </h2>
+            <div className="products-container">
+              {[...products]
+                .splice(0, 4)
+                .map((product) => (
+                <ProductCard key={product.id} product={product}/>
+              ))}
+            </div>
           </div>
-        </div>
       ))}
     </>
   )
